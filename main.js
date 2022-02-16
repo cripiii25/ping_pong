2a9fbfd90a6f030e033783cf891a7b7df47f23d0
Juan Manuel <juanlondonoierml@gmail.com>
2022-02-16 18:51:19 -0500

    Se ha agregado la animación de la pelota, y sus respectivas funciones; también se ha agregado la funcion de Star and pause

--- main.js
+++ main.js
@@ -8,11 +8,12 @@
         this.game_over = false;
         this.bars = [];
         this.ball = null;
+        this.playing = false;
     }
 
     self.Board.prototype = {
         get elements() {
-            var elements = this.bars;
+            var elements = this.bars.map(function(bar){return bar;});
             elements.push(this.ball); 
             return elements;
         }
@@ -27,9 +28,18 @@
         this.speed_y = 0;
         this.speed_x = 3;  
         this.board = board;
+        this.direction = 1;
         
         board.ball = this; 
         this.kind = "circle"; 
+    }
+
+    self.Ball.prototype ={
+        move: function(){
+            this.x += (this.speed_x * this.direction);
+            this.y += (this.speed_y);
+
+        }
     }
 })();
 //barras
@@ -42,7 +52,7 @@
         this.board = board;
         this.board.bars.push(this);
         this.kind = "rectangle";
-        this.speed = 10;
+        this.speed = 5;
     }
 
     self.Bar.prototype = {
@@ -80,8 +90,12 @@
             };
         },
         play: function(){
-           this.clean();
-           this.draw();
+            if(this.board.playing){
+                this.clean();
+                this.draw();
+                this.board.ball.move();
+            }
+           
         }
 
     }
@@ -114,22 +128,33 @@ var ball = new Ball(350,100,10,board);
 
 
 document.addEventListener("keydown", function (ev) {
-    
-    ev.preventDefault();
     if (ev.keyCode == 38) {
+        ev.preventDefault();
         bar.up();
     } else if (ev.keyCode == 40) {
+        ev.preventDefault();
         bar.down();
     }else if (ev.keyCode == 87) {
+        ev.preventDefault();
         //W
         bar_2.up();
     } else if (ev.keyCode == 83) {
+        ev.preventDefault();
         //S
         bar_2.down();
+    }else if(ev.keyCode === 32){
+        // Pausar
+        ev.preventDefault();
+        board.playing = !board.playing;
+
     }
 });
 
+ board_view.draw();
 window.requestAnimationFrame(controller);
+setTimeout(function(){
+    ball.direction = -1;
+},4000);
 
 //Controlador
 function controller() {
