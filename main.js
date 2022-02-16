a48e07475f39dc2496712ee3b6fe7f18f7d393ab
Juan Manuel <juanlondonoierml@gmail.com>
2022-02-15 19:52:56 -0500

    Se han agragado las barras del juego

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // Ejecución de todos los elementos
+//Modelo
 (function(){
         self.Board = function(width,height){
         this.width=width;
@@ -12,12 +13,37 @@
     self.Board.prototype = {
         get elements (){
            var elements = this.bars; 
-           elements.push(ball); 
+           elements.push(this.ball); 
            return elements; 
         }
     }
 
 }) ();
+
+//barras
+(function(){
+   self.Bar = function(x,y,width,height,board){
+       this.x = x;
+       this.y = y; 
+       this.width = width; 
+       this.height = height;
+       this.board = board; 
+       this.board.bars.push(this);
+       this.kind="rectangle";
+       console.log("Hola Mundo");
+   }
+
+   self.Bar.prototype ={
+       down: function(){
+
+       },
+       up:function(){
+
+       }
+   }
+})();
+
+// vista
 (function(){
     self.BoardView = function(canvas,board){
         this.canvas = canvas; 
@@ -26,13 +52,36 @@
         this.board=board; 
         this.ctx = canvas.getContext("2d");
     }
+    self.BoardView.prototype ={
+        draw:function(){
+            for(var i = this.board.elements.length - 1; i>=0; i--){
+              var el = this.board.elements[i];
+
+              draw(this.ctx, el);
+            };
+        }
+    }
+
+    function draw(ctx,element){
+        if(element !== null && element.hasOwnProperty("kind")){
+            switch(element.kind){
+                case "rectangle":
+                    ctx.fillRect(element.x,element.y,element.width,element.height);
+                    break; 
+            }  
+        }
+    }
 
 })();
+
 window.addEventListener("load",main);
 
+//Controlador
 function main(){
     var board = new Board(800,400);
+    var bar = new Bar(20,100,40,100,board);
+    var bar = new Bar(737,100,40,100,board);
     var canvas= document.getElementById('canvas');
     var board_view = new BoardView(canvas,board);
-
+    board_view.draw();
 }
